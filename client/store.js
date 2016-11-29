484ad4ababe5c103277cb4dacef2796633f13950
handsomeOne <i@handsomeone.com>
2016-11-29 13:43:01 +0800

    扁平目录结构

--- client/store.js
+++ client/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import promiseMiddleware from 'redux-promise'
 
-import rootReducer from '../reducers'
+import rootReducer from './reducers'
 
 export default function configure(initialState) {
   const create = window.devToolsExtension
