63b7cbcac80a3c529d3c472aafea052df9a8d0b7
handsomeOne <i@handsomeone.com>
2016-12-26 17:28:53 +0800

    节约一点带宽

--- server/utils/getFoldedStats.js
+++ server/utils/getFoldedStats.js
@@ -1,10 +1,7 @@
 exports.getStatuses = function getStatuses(foldedSnapshots) {
   return foldedSnapshots.map(fold => fold.reduce((statuses, snapshot) => {
+    statuses[snapshot.status] = statuses[snapshot.status] || 0
     statuses[snapshot.status] += 1
     return statuses
-  }, {
-    OK: 0,
-    Error: 0,
-    Idle: 0,
-  }))
+  }, {}))
 }
