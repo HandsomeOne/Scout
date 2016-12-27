6fb9fb0e3db6f0967b594d91074725c8a3718500
handsomeOne <i@handsomeone.com>
2016-12-27 19:11:35 +0800

    jumbotron

--- client/components/Stats/Overview.jsx
+++ client/components/Stats/Overview.jsx
@@ -1,34 +1,69 @@
-import React, { PropTypes as T } from 'react'
+import React, { Component, PropTypes as T } from 'react'
 import formatTinyTime from '../../utils/formatTinyTime'
+import { origin } from '../../config'
 import $ from './Overview.css'
 
-export default function Overview(props) {
-  return (
-    <div className={$.overview}>
+export default class Overview extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  componentDidMount() {
+    fetch(`${origin}/stats/${this.props.id}?since=${this.props.since}`)
+    .then(res => res.json())
+    .then((json) => {
+      this.setState(json)
+    })
+  }
+  render() {
+    const {
+      name,
+      URL,
+      Apdex,
+      ApdexTarget,
+      OK,
+      Error,
+      Idle,
+      meanResponseTime,
+    } = this.state
+    const total = OK + Error + Idle
+    const health = total ? (OK + Idle) / total : 0
+    return (
       <div>
-        <h6>健康度</h6>
-        {Math.round(props.health * 100)}%
+        <div className={$.overview}>
+          <div>
+            <h6>健康度</h6>
+            {Math.round(health * 100)}%
+          </div>
+          <div>
+            <h6>Apdex / {formatTinyTime(ApdexTarget)}</h6>
+            {typeof Apdex !== 'number' ? 'NaN' : Apdex.toFixed(2)}
+          </div>
+          <div>
+            <h6>平均响应时间</h6>
+            {formatTinyTime(meanResponseTime)}
+          </div>
+          <div>
+            <h6>错误数</h6>
+            {Error}
+          </div>
+        </div>
+
+        <div className={$.jumbotron}>
+          <h1>{name}</h1>
+          <p>{URL}</p>
+          <p className={$.summary}>
+          在过去的 {this.props.since / 60} 小时中，
+          接口共被检测了 {total} 次，
+          其中有 {Error} 次异常，
+          {Idle} 次处于休眠时间。</p>
+        </div>
       </div>
-      <div>
-        <h6>Apdex / {formatTinyTime(props.ApdexTarget)}</h6>
-        {props.Apdex}
-      </div>
-      <div>
-        <h6>平均响应时间</h6>
-        {formatTinyTime(props.meanResponseTime)}
-      </div>
-      <div>
-        <h6>错误数</h6>
-        {props.Error}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 Overview.propTypes = {
-  health: T.number,
-  Apdex: T.string,
-  ApdexTarget: T.number,
-  meanResponseTime: T.number,
-  Error: T.number,
+  id: T.string,
+  since: T.number,
 }
--- client/components/Stats/index.jsx
+++ client/components/Stats/index.jsx
@@ -1,38 +1,11 @@
 import React, { Component, PropTypes as T } from 'react'
-import { origin } from '../../config'
 import Overview from './Overview'
 
 class Stats extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-  componentDidMount() {
-    fetch(`${origin}/stats/${this.props.params.id}?since=1440`)
-    .then(res => res.json())
-    .then((json) => {
-      this.setState(json)
-    })
-  }
   render() {
-    const {
-      Apdex,
-      ApdexTarget,
-      OK,
-      Error,
-      Idle,
-      meanResponseTime,
-    } = this.state
-    const total = OK + Error + Idle
     return (
       <div>
-        <Overview
-          health={total ? (OK + Idle) / total : 0}
-          Apdex={typeof Apdex !== 'number' ? 'NaN' : Apdex.toFixed(2)}
-          ApdexTarget={ApdexTarget}
-          meanResponseTime={meanResponseTime}
-          Error={Error}
-        />
+        <Overview id={this.props.params.id} since={1440} />
       </div>
     )
   }
