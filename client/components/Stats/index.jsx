ead0364a4a249ef9b6159d54586e79d3f5094a7f
handsomeOne <i@handsomeone.com>
2016-12-27 19:23:43 +0800

    since 和 interval 移到 state 中

--- client/components/Stats/index.jsx
+++ client/components/Stats/index.jsx
@@ -2,10 +2,17 @@ import React, { Component, PropTypes as T } from 'react'
 import Overview from './Overview'
 
 class Stats extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      since: 1440,
+      interval: 30,
+    }
+  }
   render() {
     return (
       <div>
-        <Overview id={this.props.params.id} since={1440} />
+        <Overview id={this.props.params.id} since={this.state.since} />
       </div>
     )
   }
