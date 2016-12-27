ef8fcfa0204024c7315667a5531ffcc26b7fced6
handsomeOne <i@handsomeone.com>
2016-12-27 18:33:17 +0800

    微调样式

--- client/components/Stats/Overview.jsx
+++ client/components/Stats/Overview.jsx
@@ -5,15 +5,13 @@ import $ from './Overview.css'
 export default function Overview(props) {
   return (
     <div className={$.overview}>
-      <div />
       <div>
         <h6>健康度</h6>
         {Math.round(props.health * 100)}%
       </div>
       <div>
-        <h6>Apdex</h6>
+        <h6>Apdex / {formatTinyTime(props.ApdexTarget)}</h6>
         {props.Apdex}
-        <small>~{formatTinyTime(props.ApdexTarget)}</small>
       </div>
       <div>
         <h6>平均响应时间</h6>
@@ -23,7 +21,6 @@ export default function Overview(props) {
         <h6>错误数</h6>
         {props.Error}
       </div>
-      <div />
     </div>
   )
 }
