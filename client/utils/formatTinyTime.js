dfb4f04cbc462903e67cde61338c7dc3bbeb61d2
handsomeOne <i@handsomeone.com>
2016-12-28 15:38:22 +0800

    给 formatTinyTime 默认值

--- client/utils/formatTinyTime.js
+++ client/utils/formatTinyTime.js
@@ -1,3 +1,4 @@
-export default ms => (
-  ms < 1000 ? `${ms.toFixed(0)}ms` : `${(ms / 1000).toFixed(2)}s`
-)
+export default (milliseconds) => {
+  const ms = milliseconds || 0
+  return ms < 1000 ? `${ms.toFixed(0)}ms` : `${(ms / 1000).toFixed(2)}s`
+}
