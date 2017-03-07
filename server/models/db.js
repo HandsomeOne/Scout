0b6cea79c0316aa159760a38f34557cafa6d586a
handsomeOne <i@handsomeone.com>
2017-03-07 16:31:19 +0800

    命令行启动传输mongodb地址作为参数

--- server/models/db.js
+++ server/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/scout')
+mongoose.connect(process.argv[2] || 'mongodb://localhost/scout')
 
 module.exports = mongoose
