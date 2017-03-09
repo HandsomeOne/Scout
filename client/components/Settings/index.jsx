8bf1163a45f758154430e939facd0f276ccea744
handsomeOne <i@handsomeone.com>
2017-03-09 12:35:53 +0800

    增加‘在接口恢复时通知’配置项

--- client/components/Settings/index.jsx
+++ client/components/Settings/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes as T } from 'react'
-import { Form, Icon, Button, message } from 'antd'
+import { Form, Icon, Button, Checkbox, message } from 'antd'
 import fetch from 'isomorphic-fetch'
 import { origin } from '../../config'
 import $ from './index.css'
@@ -12,6 +12,7 @@ class Settings extends Component {
     this.state = {
       isAlertURLDetailVisible: false,
       isTesting: false,
+      settings: {},
     }
     this.submit = this.submit.bind(this)
     this.toggleAlertURLDetail = this.toggleAlertURLDetail.bind(this)
@@ -20,7 +21,7 @@ class Settings extends Component {
     fetch(`${origin}/settings`)
     .then(res => res.json())
     .then((settings) => {
-      this.setState(settings)
+      this.setState({ settings })
     })
   }
   submit() {
@@ -42,7 +43,7 @@ class Settings extends Component {
   render() {
     const { Item } = Form
     const { getFieldDecorator } = this.props.form
-    const settings = this.state
+    const { settings } = this.state
     const { getFieldValue } = this.props.form
 
     return (
@@ -64,6 +65,12 @@ class Settings extends Component {
             <AlertURLDetail alertURL={getFieldValue('alertURL')} />
           </div>
 
+          <Item>
+            {getFieldDecorator('alertOnRecovery', {
+              valuePropName: 'checked',
+              initialValue: settings.alertOnRecovery,
+            })(<Checkbox>在接口恢复时通知</Checkbox>)}
+          </Item>
           <Button
             onClick={this.submit}
             size="large"
