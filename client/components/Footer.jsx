e5eec498ed5d6c66127ae6907286659f243be423
HandsomeOne <i@handsomeone.com>
2017-03-29 13:20:53 +0800

    升级版本，增加页尾

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Layout, Icon } from 'antd'
 import { colors as C } from '../config'
+import { version } from '../../package.json'
 
 const { Footer } = Layout
 export default () => (
   <Footer style={{ textAlign: 'center' }}>
+    <a href="https://github.com/HandsomeOne/Scout" target="_blank" rel="noopener noreferrer"><Icon type="github" /> Scout v{version}</a>
+    <span style={{ color: '#ccc' }}> # </span>
     Made with Ant Design and <Icon type="heart" style={{ color: C.red }} />
+    <span style={{ color: '#ccc' }}> # </span>
+    <a href="https://github.com/HandsomeOne/Scout/issues/new" target="_blank" rel="noopener noreferrer">报告问题</a>
   </Footer>
 )
